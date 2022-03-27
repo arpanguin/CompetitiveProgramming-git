731a851701e4e7d05eb8d602c8348db56cda4fc1
Arpan Guin <arpguin@gmail.com>
2022-03-27 12:02:09 +1100

    master

--- program/FindFirstDuplicate.py
+++ program/FindFirstDuplicate.py
@@ -1,11 +1,12 @@
 def first_duplicate(array):
     for val in array:
         absValue = abs(val)
-        if array[absValue - 1] < 0:
+        if array[absValue-1] < 0:
             return absValue
-        array[absValue - 1] *= -1
+        array[absValue-1] *= -1
     return -1
 
 
 array = [2, 1, 5, 2, 3, 3, 4]
+#array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(first_duplicate(array))
